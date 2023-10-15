49008f5158498cd902390dfa43b0c35745bcfafa
Harsh <hk3711750@gmail.com>
2023-10-15 18:20:47 +0530

    check

--- src/Components/About.js
+++ src/Components/About.js
@@ -29,7 +29,7 @@ export default function About(props) {
               classNamees that we use to style each element. These classNamees control
               the overall appearance, as well as the showing and hiding via CSS
               transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
+              overriding default variables. It's also worth noting that just
               about any HTML can go within the <code>.accordion-body</code>,
               though the transition does limit overflow.
             </div>
